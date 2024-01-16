9c720274b6915fdc836b4e06cd9ecf4c4b844529
Juan Suarez <jsuarez0698@gmail.com>
2024-01-16 15:51:24 -0400

    fixing chopping cart error

--- second-folder/worldsthenics.js
+++ second-folder/worldsthenics.js
@@ -4,6 +4,7 @@ let list = document.querySelector(".list");
 let listCard = document.querySelector(".listCard");
 let body = document.querySelector("body");
 let qty = document.querySelector(".qty");
+let total = document.querySelector(".total")
 openShopping.addEventListener("click", () => {
   body.classList.add("active");
 });
@@ -46,11 +47,11 @@ let products = [
     category: "bars",
     name: "dip bars",
     price: 300,
-    image: "./worldsthenics-imgs/belts/dipBelt.webp",
+    image: "./worldsthenics-imgs/bars/dipBar.webp",
   },
 
   {
-    category: "paralelas",
+    category: "bars",
     name: "pull up bar",
     price: 170,
     image: "./worldsthenics-imgs/bars/pullupBar.webp",
@@ -71,13 +72,13 @@ let products = [
 
   {
     category: "barbell",
-    name: "20 kg male barbell",
+    name: "20 kg barbell",
     price: 17,
     image: "./worldsthenics-imgs/barbells/male20kgBar.webp",
   },
   {
     category: "barbell",
-    name: "15 kg female barbell",
+    name: "15 kg barbell",
     price: 25,
     image: "./worldsthenics-imgs/barbells/female15kgBar.jpg",
   },
@@ -88,17 +89,58 @@ let listProducts = [];
 function initShop() {
   products.forEach((value, key) => {
     let productsDiv = document.createElement("div");
+    productsDiv.classList.add("item");
     productsDiv.innerHTML = `
             
             
             <h1>${value.name}</h1>
             <img classname="card-photo" src="${value.image}"/>
-            <p> ${value.price}</p>
+            <p>$ ${value.price.toLocaleString()}</p>
             <p>${value.category}</p>
-            <button><h4>Buy product<h4></button
+            <button onclick="addToCart(${key})">Buy product</button
             
         `;
     list.appendChild(productsDiv);
   });
 }
 initShop();
+
+function addToCart(key) {
+    if(listProducts[key] == null){
+        listProducts[key] = products[key];
+        listProducts[key].qty = 1;
+    }
+    reloadCard();
+}
+
+function reloadCard(){
+    listProducts.innerHTML = "";
+    let count = 0;
+    let totalPrice = 0;
+    listProducts.forEach((value, key) => {
+        totalPrice = totalPrice + value.price;
+        count = count + value.qty;
+
+        if(value != null){
+            let cartDiv = document.createElement("li");
+            cartDiv.innerHTML = `
+            <div><img src="${value.image}"/></div>
+            <div>${value.name}</div>
+            <div>${value.price.toLocaleString()}</div>
+            <div>${value.category}</div>
+            <div>${value.qty}</div>
+            <div>
+                <button onclick="changeQty(${key}, ${value.qty - 1})">-</button>
+               </div> 
+            <div classname="count">${value.qty}</div>
+            <div>
+            <button onclick="changeQty(${key}, ${value.qty + 1})">+</button>
+            </div>
+            `;
+            listCard.appendChild(cartDiv);
+        }
+    })
+
+    total.innerText = totalPrice;
+    qty.innerText = count;
+}
